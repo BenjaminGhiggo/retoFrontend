3e5ae1fea4f26ca4985ab5aea1b1b12f4f22b305
BenjaminGhiggo <victor.ghiggo.d@uni.pe>
2025-08-27 01:39:49 -0500

    Create | cramos composable de conversión

---
+++ composables/useConverter.ts
@@ -0,0 +1,21 @@
+import { ConvertCurrencyUseCase } from '~/src/application/usecases/ConvertCurrency';
+import { FirebaseExchangeRateRepository } from
+'~/src/infrastructure/adapters/FirebaseExchangeRateRepository';
+
+export const useConverter = () => {
+const repository = new FirebaseExchangeRateRepository();
+const convertCurrency = new ConvertCurrencyUseCase(repository);
+
+const convertPenToUsd = async (penAmount: number) => {
+    return await convertCurrency.convertPenToUsd(penAmount);
+};
+
+const convertUsdToPen = async (usdAmount: number) => {
+    return await convertCurrency.convertUsdToPen(usdAmount);
+};
+
+return {
+    convertPenToUsd,
+    convertUsdToPen
+};
+};
