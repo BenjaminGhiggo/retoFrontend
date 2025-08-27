3f17ec3b176bb009382145fff319eb588d6773b7
BenjaminGhiggo <victor.ghiggo.d@uni.pe>
2025-08-26 23:47:27 -0500

    Create | carpetas e interfaces

---
+++ src/domain/entities/ExchangeRate.ts
@@ -0,0 +1,13 @@
+export interface ExchangeRate { // interface para las tasas de cambio
+    purchasePrice: number; // precio de compra
+    salePrice: number; // precio de venta
+    timestamp: string; // marca de tiempo
+}
+
+export interface ConversionResult { // interface para el resultado de la conversion
+    originalAmount: number; // monto original
+    convertedAmount: number; // monto convertido
+    rate: number; // tasa de cambio utilizada
+    fromCurrency: 'USD' | 'PEN'; // moneda de origen
+    toCurrency: 'USD' | 'PEN'; // moneda de destino
+}
