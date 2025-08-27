07e4d9e102b38bbf3e8ec6c5bec0fcc13e59c05b
BenjaminGhiggo <victor.ghiggo.d@uni.pe>
2025-08-27 00:09:18 -0500

    Add | creamos servicio de conversion

---
+++ src/domain/ports/IExchangeRateService.ts
@@ -0,0 +1,6 @@
+import type { ConversionResult } from "../entities/ExchangeRate"
+
+export interface IExchangeRateService { // interface para el servicio de tasas de cambio
+    convertPenToUsd( penAmount: number): Promise<ConversionResult> //metodo para convertir de PEN a USD
+    convertUsdToPen( usdAmount: number): Promise<ConversionResult> //metodo para convertir de USD a PEN
+}
