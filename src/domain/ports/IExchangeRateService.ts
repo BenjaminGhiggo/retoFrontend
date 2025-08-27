eb28fc2449898ebea6db568774d1715f816614b8
BenjaminGhiggo <victor.ghiggo.d@uni.pe>
2025-08-27 00:27:40 -0500

    Update | correcion de formato npm run format

--- src/domain/ports/IExchangeRateService.ts
+++ src/domain/ports/IExchangeRateService.ts
@@ -1,6 +1,7 @@
-import type { ConversionResult } from "../entities/ExchangeRate"
+import type { ConversionResult } from '../entities/ExchangeRate';
 
-export interface IExchangeRateService { // interface para el servicio de tasas de cambio
-    convertPenToUsd( penAmount: number): Promise<ConversionResult> //metodo para convertir de PEN a USD
-    convertUsdToPen( usdAmount: number): Promise<ConversionResult> //metodo para convertir de USD a PEN
+export interface IExchangeRateService {
+  // interface para el servicio de tasas de cambio
+  convertPenToUsd(penAmount: number): Promise<ConversionResult>; //metodo para convertir de PEN a USD
+  convertUsdToPen(usdAmount: number): Promise<ConversionResult>; //metodo para convertir de USD a PEN
 }
